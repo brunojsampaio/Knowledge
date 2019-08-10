ee45cf2e2675cbcffe9eef48fd5359e82b9fa328
Bruno Sampaio <brunojsampaio@gmail.com>
2019-08-10 15:50:32 -0300

    Adicionado autenticação jwt

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,38 +1,56 @@
 module.exports = app => {
+  app.route("/signup").post(app.api.user.save);
+
+  app.route("/signin").post(app.api.auth.signin);
+
+  app.route("/validateToken").post(app.api.auth.validateToken);
+
   app
     .route("/users")
+    .all(app.config.passport.authenticate())
     .post(app.api.user.save)
     .get(app.api.user.get);
 
   app
     .route("/users/:id")
+    .all(app.config.passport.authenticate())
     .put(app.api.user.save)
     .get(app.api.user.getById);
 
   app
     .route("/categories")
+    .all(app.config.passport.authenticate())
     .post(app.api.category.save)
     .get(app.api.category.get);
 
   // Cuidado com ordem! Tem que vir antes de /categories/:id
-  app.route("/categories/tree").get(app.api.category.getTree);
+  app
+    .route("/categories/tree")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getTree);
 
   app
     .route("/categories/:id")
+    .all(app.config.passport.authenticate())
     .put(app.api.category.save)
     .get(app.api.category.getById)
     .delete(app.api.category.remove);
 
   app
     .route("/articles")
+    .all(app.config.passport.authenticate())
     .get(app.api.article.get)
     .post(app.api.article.save);
 
   app
     .route("/articles/:id")
+    .all(app.config.passport.authenticate())
     .get(app.api.article.getById)
     .put(app.api.article.save)
     .delete(app.api.article.remove);
 
-  app.route("/categories/:id/articles").get(app.api.article.getByCategory);
+  app
+    .route("/categories/:id/articles")
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.getByCategory);
 };
