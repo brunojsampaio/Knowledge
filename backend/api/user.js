8cd0b366561c35f50e4bee6d0ba874e28aae4362
Bruno Sampaio <brunojsampaio@gmail.com>
2019-08-10 19:25:47 -0300

    Adicionado validar cadastro de administradores

--- backend/api/user.js
+++ backend/api/user.js
@@ -12,6 +12,9 @@ module.exports = app => {
     const user = { ...req.body };
     if (req.params.id) user.id = req.params.id;
 
+    if (!req.originalUrl.startsWith("/users")) user.admin = false;
+    if (!req.user || !req.user.admin) user.admin = false;
+
     try {
       existsOrError(user.name, "Nome não informado");
       existsOrError(user.email, "E-mail não informado");
@@ -38,7 +41,7 @@ module.exports = app => {
         .db("users")
         .update(user)
         .where({ id: user.id })
-        .whereNull('deletedAt')
+        .whereNull("deletedAt")
         .then(_ => res.status(204).send())
         .catch(err => res.status(500).send(err));
     } else {
@@ -54,7 +57,7 @@ module.exports = app => {
     app
       .db("users")
       .select("id", "name", "email", "admin")
-      .whereNull('deletedAt')
+      .whereNull("deletedAt")
       .then(users => res.json(users))
       .catch(err => res.status(500).send(err));
   };
@@ -64,7 +67,7 @@ module.exports = app => {
       .db("users")
       .select("id", "name", "email", "admin")
       .where({ id: req.params.id })
-      .whereNull('deletedAt')
+      .whereNull("deletedAt")
       .first()
       .then(user => res.json(user))
       .catch(err => res.status(500).send(err));
